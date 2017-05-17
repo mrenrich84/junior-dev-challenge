ba989c4924ce851ba18c75d009300f6c3198c417
Enrico Graziani <mrenrich84@gmail.com>
2017-05-17 13:01:25 +0100

    API server: writing tests for development

---
+++ api/server.spec.js
@@ -0,0 +1,37 @@
+let server = require('./server')
+let request = require('request-promise-native')
+
+describe ("API server", () => {
+  afterAll(() => {
+    server.close()
+  })
+  describe ('/', () => {
+    it ("API server is on" , async () => {
+      // sends a request to the API
+      let options = {
+        uri:  'http://localhost:8000/',
+        resolveWithFullResponse: true,
+        simple: false
+      }
+      let res = await request(options)
+
+      // expect to receive 200
+      expect(res.statusCode).toEqual(200)
+    })
+  })
+  xdescribe ('/get_list_of_clients', () => {
+    it ("returns a list of clients" , async () => {
+      // sends a request to the API
+      let options = {
+        uri:  'http://localhost:8000/get_list_of_clients',
+        resolveWithFullResponse: true,
+        simple: false
+      }
+      let res = await request(options)
+
+      // expect to receive 200
+      expect(res.statusCode).toEqual(200)
+      // expect to find in the results some known data
+    })
+  })
+})
