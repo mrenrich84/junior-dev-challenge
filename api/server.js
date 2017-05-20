785ccb70938caf9f608cb3602f507019f08a042d
Enrico Graziani <mrenrich84@gmail.com>
2017-05-20 11:14:23 +0100

    API Server: test and interface for /client/:id
    Wrote test for /client/:id
    Exported routes to an external helper.js module
    Defined interface for /client/:id

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 let express = require('express')
 let app = express()
 let port = 8000
-let Candidates = require('../Data/candidates').Candidates
-let Clients = require('../Data/locations').Clients
+let helper = require('./helper')
 
 app.get("/", (req, res) => res.json({message: "Working!"}));
-app.get("/clients", (req, res) => res.json(Clients))
+app.get("/clients", helper.getClients)
+app.get("/client/:id", helper.getClientById)
 
 let server_handle = app.listen(port);
 console.log("API SERVER: Listening on port " + port);
--- api/server.spec.js
+++ api/server.spec.js
@@ -27,4 +27,42 @@ describe ("API server", () => {
       expect(clients[0].name).toEqual("Ale")
     })
   })
+  xdescribe ('/client/:id', () => {
+    it ("returns a list of candidates with their distance in meters and seconds to the client" , async () => {
+      // sends a request to the API
+      let options = {
+        uri:  'http://localhost:8000/client/0',
+        resolveWithFullResponse: true,
+        simple: false
+      }
+      let res = await request(options)
+
+      expect(res.statusCode).toEqual(200)
+      // res.body == {
+      //   client: {
+      //     {
+      //       name:"Ale",
+      //       postcode:"B4 7XG"
+      //     },
+      //   candidates: [
+      //       {
+      //         name: "Agnezka Seize-Soinxante-Quatre",
+      //         postcode: "B42 1QZ",
+      //         modeOfTransport: {
+      //           type: "bike",
+      //           speed: 15.5
+      //         },
+      //         meters_to_client: 215369,
+      //         seconds_to_client: 9390
+      //       },
+      //       ...
+      //     ]
+      //   }
+      // }
+      JSON.parse(res.body).candidates.forEach(candidate => {
+        expect(typeof(candidate.meters_to_client)).toEqual('number')
+        expect(typeof(candidate.seconds_to_client)).toEqual('number')
+      })
+    })
+  })
 })
