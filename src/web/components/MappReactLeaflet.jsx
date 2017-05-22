4908288485e128ebd0abfe025cad6d3a1bf032b2
Enrico Graziani <mrenrich84@gmail.com>
2017-05-22 17:45:59 +0100

    Adding pan controls to the map

--- src/web/components/MappReactLeaflet.jsx
+++ src/web/components/MappReactLeaflet.jsx
@@ -5,10 +5,16 @@ const stamenTonerTiles = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-backgro
 const stamenTonerAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 const mapCenter = [39.9528, -75.1638];
 const zoomLevel = 12;
+
 export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = { currentZoomLevel: zoomLevel };
+        this.handleUpPanClick = this.handleUpPanClick.bind(this);
+        this.handleRightPanClick = this.handleRightPanClick.bind(this);
+        this.handleLeftPanClick = this.handleLeftPanClick.bind(this);
+        this.handleDownPanClick = this.handleDownPanClick.bind(this);
+
     }
     componentDidMount() {
         const leafletMap = this.leafletMap.leafletElement;
@@ -20,22 +26,63 @@ export default class App extends Component {
     handleZoomLevelChange(newZoomLevel) {
         this.setState({ currentZoomLevel: newZoomLevel });
     }
-    render() {
-        window.console.log('this.state.currentZoomLevel ->',
-            this.state.currentZoomLevel);
-        return (
-            <div>
-                <Map
-                    ref={m => { this.leafletMap = m; }}
-                    center={mapCenter}
-                    zoom={zoomLevel}
-                >
-                    <TileLayer
-                        attribution={stamenTonerAttr}
-                        url={stamenTonerTiles}
-                    />
-                </Map>
-            </div>
-        );
+    handleUpPanClick() {
+        const leafletMap = this.leafletMap.leafletElement;
+        leafletMap.panBy([0, -100]);
+        window.console.log('Panning up');
     }
+    handleRightPanClick() {
+        const leafletMap = this.leafletMap.leafletElement;
+        leafletMap.panBy([100, 0]);
+        window.console.log('Panning right');
+    }
+    handleLeftPanClick() {
+        const leafletMap = this.leafletMap.leafletElement;
+        leafletMap.panBy([-100, 0]);
+        window.console.log('Panning left');
+    }
+    handleDownPanClick() {
+        const leafletMap = this.leafletMap.leafletElement;
+        leafletMap.panBy([0, 100]);
+        window.console.log('Panning down');
+    }
+
+    render() {
+    window.console.log('this.state.currentZoomLevel ->',
+        this.state.currentZoomLevel);
+    return (
+        <div>
+            <Map
+                ref={m => { this.leafletMap = m; }}
+                center={mapCenter}
+                zoom={zoomLevel}
+            >
+                <TileLayer
+                    attribution={stamenTonerAttr}
+                    url={stamenTonerTiles}
+                />
+            </Map>
+            <div>
+                <div style={{ marginLeft: '37px' }}>
+                    <button onClick={this.handleUpPanClick}>
+                        Pan up
+                    </button>
+                </div>
+                <div>
+                    <button onClick={this.handleLeftPanClick}>
+                        Pan left
+                    </button>
+                    <button onClick={this.handleRightPanClick}>
+                        Pan right
+                    </button>
+                </div>
+                <div style={{ marginLeft: '30px' }}>
+                    <button onClick={this.handleDownPanClick}>
+                        Pan down
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+  }
 }
