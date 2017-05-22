1f11736740e341b185b9b3120435d877f275b20f
Enrico Graziani <mrenrich84@gmail.com>
2017-05-22 22:58:55 +0100

    PanControlContainer: increasing readability

--- src/web/components/Mapp/PanControlContainer.jsx
+++ src/web/components/Mapp/PanControlContainer.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Control from 'react-leaflet-control';
 import ControlArrow from './ControlArrow'
-
 import {  STATIC_PATH } from '../../../shared/config'
+
+const iconPath = STATIC_PATH + "/pictures/icons/arrows/"
+const iconSuffix = ".png"
+const iconSize = 24
+const marginLeft = '21px'
 
 const PanControlContainer = (props) => {
   const arrowOpts = {}
@@ -10,22 +14,22 @@ const PanControlContainer = (props) => {
   directions.forEach ( dir => {
     arrowOpts[dir] = {
       clickHandle: () => props.handlePan(dir),
-      icon: STATIC_PATH + "/pictures/icons/arrows/" + dir + ".png",
-      iconSize: 24
+      icon: iconPath + dir + iconSuffix,
+      iconSize: iconSize
     }
   })
 
   return (
     <Control position={props.position}>
       <div>
-        <div style={{ marginLeft: '21px' }}>
+        <div style={{ marginLeft: marginLeft }}>
           <ControlArrow  {...arrowOpts.up} />
         </div>
         <div>
           <ControlArrow {...arrowOpts.left} />
           <ControlArrow {...arrowOpts.right}  />
         </div>
-        <div style={{ marginLeft: '21px' }}>
+        <div style={{ marginLeft: marginLeft }}>
           <ControlArrow {...arrowOpts.down} />
         </div>
       </div>
