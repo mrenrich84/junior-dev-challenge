7e5d3c6e7366f0f12acf3970995e07aff1b50677
Enrico Graziani <mrenrich84@gmail.com>
2017-05-22 12:11:19 +0100

    Web server: merge index.html into to example app

--- src/web/render-app.js
+++ src/web/render-app.js
@@ -2,15 +2,27 @@ import {  APP_NAME, APP_CONTAINER_CLASS, STATIC_PATH, WDS_PORT } from '../shared
 import { isProd } from '../shared/util'
 
 const renderApp = () =>
-`<!doctype html>
-<html>
+`
+<!DOCTYPE html>
+<html lang="en">
   <head>
+    <!-- http://colorhunt.co/c/70716 -->
+    <meta name="robots" content="noindex">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
     <title>${APP_NAME}</title>
-    <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="${STATIC_PATH}/css/materialize.min.css">
+    <link rel="stylesheet" href="${STATIC_PATH}/css/leaflet.css">
   </head>
   <body>
     <div class="${APP_CONTAINER_CLASS}"></div>
+    <!--  Scripts-->
     <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`}/js/bundle.js"></script>
+    <script src="${STATIC_PATH}/js/libs/jquery-2.1.1.min.js"></script>
+    <script src="${STATIC_PATH}/js/main.js"></script>
+    <script src="${STATIC_PATH}/js/libs/materialize.min.js"></script>
+	  <script src="${STATIC_PATH}/js/libs/leaflet.js"></script>
   </body>
 </html>
 `
