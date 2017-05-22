1e424319dfe8f7bc9fb59313149eb8dd45c8f702
Enrico Graziani <mrenrich84@gmail.com>
2017-05-22 11:51:27 +0100

    Installed an example Express+React+Webpack app

---
+++ src/web/server.js
@@ -0,0 +1,22 @@
+import compression from 'compression'
+import express from 'express'
+
+import { STATIC_PATH, WEB_PORT } from '../shared/config'
+import { isProd } from '../shared/util'
+import renderApp from './render-app'
+
+const app = express()
+
+app.use(compression())
+app.use(STATIC_PATH, express.static('dist'))
+app.use(STATIC_PATH, express.static('public'))
+
+app.get('/', (req, res) => {
+  // console.log("Received request");
+  res.send(renderApp())
+})
+
+app.listen(WEB_PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' : '(development)'}.`)
+})
