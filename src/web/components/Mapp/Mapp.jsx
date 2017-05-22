67c9bdd4e050b20844c467d855cc7b0dc81e54da
Enrico Graziani <mrenrich84@gmail.com>
2017-05-22 23:45:36 +0100

    MarkerContainer: fixed placeholder

--- src/web/components/Mapp/Mapp.jsx
+++ src/web/components/Mapp/Mapp.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Map, Popup, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
 
 import PanControlContainer from './PanControlContainer'
 import MarkerContainer from './MarkerContainer'
