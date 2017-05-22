ca208fa11e96e39b786627d7fd82acab3e4516f5
Enrico Graziani <mrenrich84@gmail.com>
2017-05-22 17:01:09 +0100

    Revert "Web server: trying to install the react-leaflet library but no working"
    This reverts commit d6c9049b46d484cee60dd3f107514aa810a6b57e.

--- src/web/components/ClientList.jsx
+++ src/web/components/ClientList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import MappReactLeaflet from './MappReactLeaflet'
+import Mapp from './Mapp'
 import Client from './Client'
 
 const ClientList = () => (
@@ -8,7 +8,7 @@ const ClientList = () => (
       <div className="col s12">
         <h2>Clients</h2>
     </div>
-    <MappReactLeaflet />
+    <Mapp />
     <div className="row">
       <div className="col s12">
         <ul className="collection">
